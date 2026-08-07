09ad76ee7f57a69c73eb4f2d2027c7b0c7910753
Yawar121 <128232578+Yawar121@users.noreply.github.com>
2026-08-07 20:53:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,70 @@
-document.getElementById("beginBtn").onclick=function(){
+const card = document.getElementById("card");
 
-alert("Welcome ❤️");
+document.getElementById("beginBtn").onclick = function () {
+
+card.innerHTML = `
+
+<h1>🥰</h1>
+
+<h2>Do you think I care about you?</h2>
+
+<p>Choose wisely 😄</p>
+
+<div class="buttons">
+
+<button onclick="nextPage()">Yes ❤️</button>
+
+<button onclick="nextPage()">Absolutely ❤️</button>
+
+</div>
+
+`;
 
 };
+
+function nextPage(){
+
+card.innerHTML = `
+
+<h1>💖</h1>
+
+<h2>Aww...</h2>
+
+<p>You haven't seen anything yet.</p>
+
+<button onclick="loveMeter()">Continue ❤️</button>
+
+`;
+
+}
+
+function loveMeter(){
+
+card.innerHTML = `
+
+<h1>❤️</h1>
+
+<h2>How Much Do I Love You?</h2>
+
+<p>Move the slider...</p>
+
+<input
+type="range"
+min="0"
+max="100"
+value="0"
+id="slider"
+oninput="updateLove(this.value)"
+>
+
+<h2 id="value">0%</h2>
+
+`;
+
+}
+
+function updateLove(v){
+
+document.getElementById("value").innerHTML = v + "%";
+
+}
